85a325f22053afcd0f2c6743acc6befb2219a066
Viktor <belinchio@yandex.ru>
2024-12-15 21:21:06 +0300

    created simple WebSocket-server

---
+++ server.js
@@ -0,0 +1,16 @@
+const WebSocket = require("ws");
+
+const wsServer = new WebSocket.Server({port: 8080});
+
+wsServer.on("connection", (socket) => {
+    console.log("Установленно новое соединение!");
+    socket.on("message", (message) => {
+        console.log(`Получено новое сообщение: ${message}`);
+        socket.send(`Эхо: ${message}`);
+    })
+    socket.close("close", () => {
+        console.log("Соединение закрыто!");
+    });
+});
+
+console.log("WebSocket-сервер запущен на порту 8080");
